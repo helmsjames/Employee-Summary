78f7b1fc5888027272ba23d0d161f2d11c1850f7
James Helms <helms.james@gmail.com>
2020-03-20 20:55:33 -0500

    passed all the tests

--- Develop/test/Intern.test.js
+++ Develop/test/Intern.test.js
@@ -3,6 +3,7 @@ const Intern = require("../lib/Intern");
 test("Can set school via constructor", () => {
   const testValue = "UCLA";
   const e = new Intern("Foo", 1, "test@test.com", testValue);
+  console.log(e.getName())
   expect(e.school).toBe(testValue);
 });
 
